a89320bd7139571ed6602b9daaba1cc46b655123
Ragaa Aly <ragaa.aly20@gmail.com>
2022-10-24 01:41:30 +0200

    Added initial admin frontend UI

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CreateAdmin from "./pages/CreateAdmin";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/createadmin' element={<CreateAdmin />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+
+    </>
+
+  );
+}
+
+export default App;
