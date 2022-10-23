b9915e7f7aafb02205e47f4b407e2bc6705bf599
Ragaa Aly <ragaa.aly20@gmail.com>
2022-10-23 13:38:35 +0200

    Error & exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 
 const app = express();
 
+app.use(express.json()) // This is a middleware function that allows us to accept JSON data in the body
+app.use(express.urlencoded({ extended: false })) // This is a middleware function that allows us to accept form data
 app.use('/api/admin', require('./routes/adminRoutes'))
 
+app.use(errorHandler)
 
 app.listen(port, () => { 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold)
