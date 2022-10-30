0604f756c3566b770631414afcd3534df3d06df3
Ragaa Aly <ragaa.aly20@gmail.com>
2022-10-30 19:23:44 +0200

    Add subtitleController and routes

---
+++ backend/controllers/subtitleController.js
@@ -0,0 +1,43 @@
+const asyncHandler = require('express-async-handler')
+const { builtinModules } = require('module')
+const Course = require('../models/courseModel')
+const Instructor = require('../models/instructorModel')
+const Subtitle = require('../models/subtitleModel')
+const Task = require('../models/taskModel')
+
+//Mehod for adding a subtitle for a certain course 
+const addSubtitle = asyncHandler(async (req, res) => {
+
+    //getting the id of the instructor 
+    const instructorid = '635a591011ecdc081ce890f7'   //Static for the sake of sprint 1 and that we do not have any authentication technique implemented
+
+    //attribute for subtitles (req.body) and course id from params
+    const courseid = req.params.courseid   //getting the id of the course  //we can send it as in body req.body
+    const title = req.body.title
+    const totalHours = req.body.totalHours
+
+    //Create the subtitle
+    const subtitle = new Subtitle({
+        title: title,
+        totalHours: totalHours,
+        course: courseid
+
+
+    })
+    //add it to the subtitle coolections (.save())
+    subtitle.save(function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+
+    //add its id to to the specified course's array of subtitles (array of subtitles' ids)
+    const newSubtitleid = [subtitle._id]
+    const newSubtitleArray = (await Course.findById(courseid)).subtitles.concat(newSubtitleid)
+
+    const updateCourseSubtitles = await Course.findByIdAndUpdate(courseid, { subtitles: newSubtitleArray }, { new: true })
+    res.json(subtitle)
+
+})
+
+module.exports = { addSubtitle}
--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ app.use('/api/courses', require('./routes/courseRoutes'))
 app.use('/api/instructor',require('./routes/instructorRoutes'))
 app.use('/api/guest',require('./routes/guestRoutes'))
 app.use('/api/corporateTrainee',require('./routes/corporateTraineeRoutes'))
-app.use('/api/individualTrainee',require('./routes/individualTraineeRoutes'))
+app.use('/api/individualTrainee', require('./routes/individualTraineeRoutes'))
+app.use('/api/subtitles', require('./routes/subtitleRoutes'))
 
 
 
