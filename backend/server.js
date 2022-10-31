68a822737f9c3fd3224d24a8a7da60e5a38cc4af
Nada Farid <nada17.aiman@gmail.com>
2022-10-31 02:08:36 +0200

    Testing Frontend by HTML

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,19 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const bodyParser = require("body-parser");
 
 connectDB()
 
-
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.json()) // This is a middleware function that allows us to accept JSON data in the body
 app.use(express.urlencoded({ extended: false })) // This is a middleware function that allows us to accept form data
 app.use(errorHandler)
 
 app.use('/api/admin', require('./routes/adminRoutes'))
-
 
 
 app.listen(port, () => { 
---
+++ frontend/test.js
@@ -0,0 +1,5 @@
+function getUser(){
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    document.getElementById("demo").innerHTML = fname + " " + lname;
+}
