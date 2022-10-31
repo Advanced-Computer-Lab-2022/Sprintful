19fd92eda13fa74cd964e4cf848b2a7bdf64fce9
Ragaa Aly <ragaa.aly20@gmail.com>
2022-11-01 00:39:16 +0200

    Add some lines

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 // import JSONDATA from "./MOCK_DATA.json"
+import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+
 import "./App.css"
 import Searchbar from "./components/SearchBar";
 function App() {
   return(
     <div classname="App">
-      <Searchbar placeholder="Enter a course title or subject or instructor ..." />
+      <BrowserRouter>
+      <div className='pages'>
+        <Routes>
+            <Route exact path="/api/courses/search" element={
+              <Searchbar placeholder="Enter a course title or subject or instructor ..." />
+            } />
+        </Routes>
+      </div>
+      </BrowserRouter>
+
     </div>
   );
 }
