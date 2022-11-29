feb316fa167f76bf01c555d514724d6f0a9b7b60
Ragaa Aly <ragaa.aly20@gmail.com>
2022-11-29 22:43:55 +0200

    Add initial quiz files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Course from './pages/Course'
+import Quiz from './pages/Quiz'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/api/courses/" element={<Course />} />
+          <Route path="/courses/" element={<Course />} />
+          <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </BrowserRouter>
     </>
