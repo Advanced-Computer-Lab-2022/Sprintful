fba5b2bf47d017e68672f3bf72e37310b4bcc06e
Reem <reemelsady2152@gmail.com>
2022-12-04 23:17:26 +0200

    View subtitlePage for Instructor Frontend/NoRoutes

---
+++ frontend/src/pages/SubtitlePageInst.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+
+
+
+const SubtitlePageInst= ()=>{
+    const[content,setContent]=useState('');
+    const [title,setTitle]=useState('')
+    const [totalHours,setTotalHours]=useState(0);
+    const [videoLink,setVideoLink]=useState("");
+    const [videoDescription,setVideoDescription]=useState("");
+
+    //params 
+    const params=useParams();
+    const courseid=params.courseid;
+    const subtitleid=params.subtitleid;
+
+
+//function for fetching subtitle data
+    const fetchdata=async()=>{
+    
+        
+        const response =await axios.get(`/api/subtitles/${courseid}/${subtitleid}`)
+        const subtitle=response.data;
+        setContent(subtitle.content);
+        setTitle(subtitle.title);
+        setTotalHours(subtitle.totalHours );
+        setVideoLink(subtitle.youtubevideo);
+        setvideoDescription(subtitle.videoDescription)
+    }
+
+// useEffect for fetching the data in the very 1st render 
+    useEffect(fetchdata,[]);
+
+
+    //title
+    // let title='';
+    // //content 
+    // let content=''; //it is not a state because we do not need to keep a track of it (it will not change based on sth. the user done)
+    // // total Hours 
+    // let  totalHours=0;
+    // //Youtube video Link
+    // let videoLink='';
+    // //video Description 
+    // let videoDescription='';
+
+
+
+
+    return (
+
+    <div>
+        <h2>{title}</h2>
+        <label>Total hours : {totalHours}</label>
+        <h3>Subtitle Content :</h3>
+        <p>{content}</p>
+
+        <br></br>
+        <br></br>
+
+        <a href={videoLink}>Click to view Youtube Video</a>
+
+        <label>Video Description :</label>
+        <br></br>
+        <p>{videoDescription}</p>
+
+
+
+
+    </div>
+
+
+
+    )
+
+
+
+}
