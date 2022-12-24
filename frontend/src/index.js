9fa555f2631f4cd4527ebb20fc9289aa0c1458fb
Reem <reemelsady2152@gmail.com>
2022-12-24 11:02:07 +0200

    installed some frontend ui packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,12 @@ import App from './App';
 // import './index.css';
 // import "./styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
