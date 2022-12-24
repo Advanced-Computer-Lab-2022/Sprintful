cab138765cdf66bddc4a85f87a717f857c85f943
Reem <reemelsady2152@gmail.com>
2022-12-24 18:18:10 +0200

    CoursePageView FE without any styling

--- frontend/src/pages/CourseView.js
+++ frontend/src/pages/CourseView.js
@@ -13,36 +13,7 @@ const CourseView=()=>{
     const {courseid}=useParams();
 
 
-    //Fetching the course's data and its Subtitles
-    //   const getCourseanditsSubtitle=async()=>{
-
-       
-    //     try{
-    //     //Sending a get request to the server to get course
-    //        const response= await axios.get('/api/courses/',{params :{id:courseid}});
-    //        const coursedata=response.data;
-    //        setCourse(coursedata);
-
-    //        //handling setting course price according to discount and its expiry date 
-
-
-
-    //        //Sending a get request to server to get this course's Subtitles
-
-
-
-
-
-    //     }
-    //     //catching any request error
-    //     catch (error){
-
-    //     }
-
-    //   }
-
-
-
+  
     //Using useEffect to run only on 1st render to display the course's data
     useEffect( ()=>{
         const getCourseanditsSubtitle=async()=>{
@@ -85,13 +56,11 @@ const CourseView=()=>{
 
 
 
+            //Sending a get request to server to get this course's Subtitles
+            const response2=await axios.get(`http://localhost:5000/api/courses/getSubtitlesforCourse/${courseid}`);
+            const subtitlesArray=response2.data;
+            setCourseSubtitles(subtitlesArray);
 
-
-
-    
-    
-               //Sending a get request to server to get this course's Subtitles
-    
     
     
     
@@ -112,22 +81,52 @@ const CourseView=()=>{
 
 
     return (
-    <div>
-        {/* we check that course is not null before getting its attributes using boolean operator && AND */}
-        <h1>{course&&course.title}</h1>
-        <h2>Total Hours :{course&&course.totalhours}</h2>
-        <h4>Price:  {course&&coursePriceAfterDiscount}</h4>
+
+        <div>
+            <div>
+               {/* we check that course is not null before getting its attributes using boolean operator && AND */}
+                <h1>{course&&course.title}</h1>
+                <h2>Total Hours :{course&&course.totalhours}</h2>
+                <h4>Price:  {course&&coursePriceAfterDiscount}</h4>
+
+             </div>
 
 
 
+
+             
+
+              <div>
+                 {/* subtitles */}
+                 
+    
+                  {courseSubtitles.map((subtitle)=>(
+                    <div  key={subtitle._id}>
+                       <h5>{subtitle.title}</h5>
+                       <h6>total hours :{subtitle.totalHours}</h6>
+                       {subtitle.tasks.map((task)=>(
+                        <div key={task._id}>
+                          <p>  {task.title}</p>
+                        </div>
+
+                       ))}
+
+                    </div>
+
+
+                  ))}
+
+
+
+
+
+
+
+             </div>
 
 
 
     </div>
-
-
-
-
     )
 }
 
