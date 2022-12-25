b94d3f74caa87d4165d3f29b9a5b2294e62dd451
Nada Farid <nada17.aiman@gmail.com>
2022-12-26 01:01:58 +0200

    Styling

--- frontend/src/pages/InstructorProfile.jsx
+++ frontend/src/pages/InstructorProfile.jsx
@@ -139,11 +139,6 @@ export default function InstructorProfile() {
                      className="rounded-circle img-fluid" style={{width: "150px"}}/>
                    <h5 className="my-3">John Smith</h5>
                    <p className="text-muted mb-1">Rating: {instructor.rating}</p>
-                   <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
-                   <div className="d-flex justify-content-center mb-2">
-                     <button type="button" className="btn btn-primary">Follow</button>
-                     <button type="button" className="btn btn-outline-primary ms-1">Message</button>
-                   </div>
                  </div>
                </div>
              </div>
