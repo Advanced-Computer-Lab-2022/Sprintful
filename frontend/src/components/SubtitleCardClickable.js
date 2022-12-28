ad6a24e5ef8553cc2e65f4dbc509e7fc16835c5a
Reem <reemelsady2152@gmail.com>
2022-12-28 20:52:51 +0200

    SubtitleCardclickable Initialization

---
+++ frontend/src/components/SubtitleCardClickable.js
@@ -0,0 +1,43 @@
+import {StyleCard} from './styles/Card.style'
+import {Link} from 'react-router-dom'
+
+export default function  SubtitleCard({subtitle:{_id,title ,totalHours, tasks}}){
+ 
+
+    return (
+        <StyleCard>
+            <div>
+
+            <h5>{title}</h5>
+            <h6>total hours :{totalHours}</h6>
+            <br></br>
+            <h6>Exercises:</h6>
+            {tasks.map((task)=>(
+                    
+                        <div key={task._id}>
+                        
+
+                            
+                         <div>
+                          
+                          <li>{task.title}</li>
+                      
+                          </div>
+                       
+                          
+                        </div>
+                     
+                      
+
+                       ))}
+
+          
+
+
+
+           </div>
+
+
+        </StyleCard>
+    )
+}
