137cbc176a51b0cdc1e734d3ebaefcf0bc18ea22
Reem <reemelsady2152@gmail.com>
2022-12-30 21:03:41 +0200

    exported refundRequestRoutes

--- backend/routes/refundRequestRoutes.js
+++ backend/routes/refundRequestRoutes.js
@@ -3,3 +3,4 @@ const router = express.Router();
 const{addRefundRequest}=require('../controllers/refundRequestController')
 
 router.post('/requestRefund/:traineeid/:courseid',addRefundRequest);
+module.exports=router;
