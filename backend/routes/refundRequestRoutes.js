599e78545c1b6aae5a272b827cc8e2c9786c8a1b
Reem <reemelsady2152@gmail.com>
2023-01-01 11:13:04 +0200

    edited refundre routes

--- backend/routes/refundRequestRoutes.js
+++ backend/routes/refundRequestRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const{addRefundRequest,acceptRefund}=require('../controllers/refundRequestController')
 //only for individual Trainee
 router.post('/requestRefund/:traineeid/:courseid',addRefundRequest);
-router.post('/AcceptRefund/:refundid',acceptRefund);
+router.post('/AcceptRefund/:refundid/',acceptRefund);
 module.exports=router;
