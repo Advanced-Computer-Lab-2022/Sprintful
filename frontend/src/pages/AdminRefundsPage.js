3db193298ed4fe1e7da83a809243aa64a09c87b1
Reem <reemelsady2152@gmail.com>
2023-01-01 14:02:35 +0200

    Admin RefundsPage partial implementation

--- frontend/src/pages/AdminRefundsPage.js
+++ frontend/src/pages/AdminRefundsPage.js
@@ -8,9 +8,30 @@ import { Box } from '@mui/system';
 import { useNavigate } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import {StyledCourseHeader} from '../components/styles/CourseHeader.style'
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import { useEffect, useState } from 'react';
+
 
 
 const AdminRefundsPage=()=>{
+
+    const [RefundsRequests,setRefundsRequests]=useState([]);
+
+    useEffect(()=>{
+        const getRefundsRequests=async()=>{
+
+            const response=await axios.get('http://localhost:5000/api/refund/getRefundsRequests');
+            const requests=response.data;
+            setRefundsRequests(requests);
+
+
+        }
+
+         getRefundsRequests();
+
+
+        },[])
 
 
 return (
@@ -18,25 +39,41 @@ return (
 
 <div>
              < StyledCourseHeader>
-                <h5>Refunds Requests :</h5>
+                <h2>Refunds Requests :</h2>
                 </StyledCourseHeader>
-             {/* {subtitle&&subtitle.tasks.map((task)=>( */}
+             {RefundsRequests && RefundsRequests.map((request)=>( 
                     
-                <div key="1">
-                    <Card sx={{ maxWidth: 400 ,maxHeight:60, mb:2}}  style={{boxShadow: "3"}}>
+                <div key={request._id}>
+                    <Card sx={{ maxWidth: 400 ,minHeight:170, mb:2}}  style={{boxShadow: "3"}}>
                          <CardContent>
-                            <Typography gutterBottom variant="h6" component="div">
-                                   I am A refund
+                            <Typography gutterBottom variant="h5" component="div">
+                                   Trainee Name :{request.traineeName}
                             </Typography>
+                            <br></br>
+                            <Typography utterBottom variant="h5" component="div">
+                                Course ID:{request.course}
+                             </Typography>
+                                <br></br>
+
+                            <Typography utterBottom variant="h5" component="div">
+                                Refund Amount :{request.amount} L.E
+                            </Typography>    
+
                             <br></br>
                             <br></br>
                             </CardContent>
+                            <CardActions>
+                              <Button size="Large">
+                                <h5>Accept</h5>
+                                </Button>
+      
+                             </CardActions>
                     </Card>
                 </div>
                  
                 
 
-                   {/* ))} */}
+                    ))} 
 
 
 
