6bb8390b6bf95054f47e6b53321bd80481166256
Reem <reemelsady2152@gmail.com>
2023-01-05 03:02:01 +0200

    update SubtitlesViews- include the array of videos

--- frontend/src/pages/SubtitleViewIT.js
+++ frontend/src/pages/SubtitleViewIT.js
@@ -137,31 +137,45 @@ export default function SubtitleViewIT() {
 
              </div>
 
-
-
              <div>
              < StyledCourseHeader>
                 <h5>Video:</h5>
                 </StyledCourseHeader>
 
+
+
+                {subtitle && subtitle.videos.map((video)=>(
+                    <div key={video._id}>
                 <Card sx={{ maxWidth: 400 ,maxHeight:60, mb:2}}  style={{boxShadow: "3"}}>
                          <CardContent>
-                         <span  onClick={()=>{handleClickOnVideo(subtitle.youtubevideo)}} style={{textDecoration: "underline",cursor: "pointer", color: "#a4243b"}}>
+                         <span onClick={()=>{handleClickOnVideo(video.youtubevideo)}} style={{textDecoration: "underline",cursor: "pointer", color: "#a4243b"}}>
+                         <Typography gutterBottom variant="h6" component="div">
+                                Video
+                                
+                            </Typography>
+                           </span>
+                         {/* <a href={subtitle&&subtitle.youtubevideo}>
                             <Typography gutterBottom variant="h6" component="div">
                                 Video
                             </Typography>
-                            </span>
+                            </a> */}
                             <Typography gutterBottom variant="h6" component="div">
-                               Video Description :{subtitle && subtitle.videoDescription}
+                               Video Description :{subtitle && video.videoDescription}
                             </Typography>
 
                             <br></br>
+                            <Typography gutterBottom variant="h6" component="div">
+                               Hours :{subtitle && video.totalNoofHours}
+                            </Typography>
                             <br></br>
                             </CardContent>
                     </Card>
+                    </div>   
+))}
 
-             </div >
-                
+               
+
+</div>
 
     </div>
   )
